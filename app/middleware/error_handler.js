d199555e715023715a3d28ccb1f7aa538c863b55
dingdongzhao <dingdongzhao@didiglobal.com>
2021-10-07 22:20:58 +0800

    去除中间件中的事务处理

--- app/middleware/error_handler.js
+++ app/middleware/error_handler.js
@@ -1,26 +1,19 @@
-"use strict";
+'use strict';
 // 请求和返回日志输出
 module.exports = () => {
   return async function errorHandler(ctx, next) {
     try {
       await next();
-      const transaction = await ctx.app.getTransaction();
-
-      // 如果有事务自动提交
-      if (transaction) {
-        transaction.commit();
-        ctx.app.deleteTransaction();
-      }
     } catch (err) {
       // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
-      ctx.app.emit("error", err, ctx);
+      ctx.app.emit('error', err, ctx);
 
       const status = err.status || 500;
 
       // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
       const error =
-        status === 500 && ctx.app.config.env === "prod"
-          ? "系统内部错误"
+        status === 500 && ctx.app.config.env === 'prod'
+          ? '系统内部错误'
           : err.message;
       ctx.body = {
         code: ctx.ERROR_CODE,
@@ -32,13 +25,6 @@ module.exports = () => {
       }
 
       ctx.status = status;
-
-      const transaction = await ctx.app.getTransaction();
-      // 如果有事务自动回滚
-      if (transaction) {
-        transaction.rollback();
-        ctx.app.deleteTransaction();
-      }
     }
   };
 };

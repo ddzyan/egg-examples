d141b371ca8cb95374ca5e1b37253f17c787f912
dingdongzhao <ddzyan@163.com>
2022-04-15 17:05:57 +0800

    docs(Rule使用优化): 📃

--- app/controller/student.js
+++ app/controller/student.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const Controller = require("../core/base_controller");
-const { createRule, queryRule } = require("../rule/student");
+const studentRule= require("../rule/student");
+
 class StudentController extends Controller {
   async create() {
     const { ctx } = this;
 
-    ctx.validate(createRule);
+    ctx.validate(studentRule.createRule);
 
     const { username, type, status } = ctx.request.body;
 
@@ -21,7 +22,7 @@ class StudentController extends Controller {
 
   async get() {
     const { ctx } = this;
-    ctx.validate(queryRule, ctx.params);
+    ctx.validate(studentRule.queryRule, ctx.params);
     const { id } = ctx.params;
 
     const res = await ctx.service.student.get(id);
